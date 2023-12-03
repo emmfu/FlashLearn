e5e9896384b101d8846e793f1fa505de3aabfc04
Jose E. Fuentes <efuenteswk@gmail.com>
2023-12-03 12:38:35 -0500

    Initial Commit

---
+++ FlashLearnCards/src/main/java/com/efu/FlashLearnCards/FlashLearnCardsApplication.java
@@ -0,0 +1,13 @@
+package com.efu.FlashLearnCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashLearnCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashLearnCardsApplication.class, args);
+	}
+
+}
---
+++ FlashLearnCards/src/test/java/com/efu/FlashLearnCards/FlashLearnCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efu.FlashLearnCards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashLearnCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
